e2b9f13d4b27f464887d28cebda4a1c8f6cd32b0
TechKnux <techlinuxpc@proton.me>
2025-12-24 16:28:40 -0300

    Minor changes to some configs

--- scripts/install_niri_cachyos.sh
+++ scripts/install_niri_cachyos.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-paru -S niri mako fuzzel brightnessctl otf-font-awesome otf-codenewroman-nerd apple-fonts thunar thunar-volman thunar-archive-plugin thunar-media-tags-plugin thunar-vcs-plugin\
- thunar-shares-plugin flameshot xdg-desktop-portal-gnome waypaper mpvpaper waybar gnome-keyring polkit-gnome xwayland-satellite sunsetr bibata-cursor-theme niri-float-sticky\
- qt6ct-kde qt5ct-kde niri_window_buttons
- 
+paru -S niri mako fuzzel brightnessctl nwg-look adw-gtk-theme kvantum kvantum-qt5 kvantum-theme-libadwaita-git otf-font-awesome otf-codenewroman-nerd\
+ apple-fonts thunar thunar-volman thunar-archive-plugin thunar-media-tags-plugin thunar-vcs-plugin thunar-shares-plugin flameshot\
+ xdg-desktop-portal-gnome waypaper mpvpaper waybar gnome-keyring polkit-gnome xwayland-satellite sunsetr bibata-cursor-theme niri-float-sticky\
+ qt6ct-kde qt5ct-kde niri_window_buttons wttrbar
+  
 
--- scripts/reclock-nouveau.sh
+++ scripts/reclock-nouveau.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 sudo touch /etc/modprobe.d/90-nouveau-reclock.conf
-echo 'options nouveau.config=NvClkMode=15' | sudo tee /etc/modprobe.d/90-nouveau-reclock.conf > /dev/null
+echo 'options nouveau config=NvClkMode=15' | sudo tee /etc/modprobe.d/90-nouveau-reclock.conf > /dev/null
+sudo limine-mkinitcpio -P
