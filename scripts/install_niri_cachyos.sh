7ee6dc5d2d3391e9df5b784bddd006b6bf3b5c0f
TechKnux <techlinuxpc@proton.me>
2025-12-24 15:31:02 -0300

    New scripts

---
+++ scripts/install_niri_cachyos.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+paru -S niri mako fuzzel brightnessctl otf-font-awesome otf-codenewroman-nerd apple-fonts thunar thunar-volman thunar-archive-plugin thunar-media-tags-plugin thunar-vcs-plugin\
+ thunar-shares-plugin flameshot xdg-desktop-portal-gnome waypaper mpvpaper waybar gnome-keyring polkit-gnome xwayland-satellite sunsetr bibata-cursor-theme niri-float-sticky\
+ qt6ct-kde qt5ct-kde niri_window_buttons
+ 
+
---
+++ scripts/reclock-nouveau.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+sudo touch /etc/modprobe.d/90-nouveau-reclock.conf
+echo 'options nouveau.config=NvClkMode=15' | sudo tee /etc/modprobe.d/90-nouveau-reclock.conf > /dev/null
